964e0e367a518806dcaf7480ea110f3c6c9635a6
Kevin Nicolas Ruiz Camargo <kevin.ruiz1811@gmail.com>
2022-05-08 12:19:35 -0500

    Organización de archivos y persistencia localStorage

---
+++ src/App/AppUI.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { TodoCounter } from "../TodoCounter";
+import { TodoSearch } from "../TodoSearch";
+import { TodoList } from "../TodoList";
+import { TodoItem } from "../TodoItem";
+import { CreateTodoButton } from "../CreateTodoButton";
+
+function AppUI({
+  totalTodos,
+  completedTodos,
+  searchvalue,
+  setSearchValue,
+  searchedTodos,
+  completeTodo,
+  deleteTodo,
+}) {
+  return (
+    <React.Fragment>
+      <TodoCounter total={totalTodos} completed={completedTodos} />
+      <TodoSearch searchvalue={searchvalue} setSearchValue={setSearchValue} />
+
+      <TodoList>
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onConplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
+  );
+}
+
+export { AppUI };
